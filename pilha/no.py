47bb4a8b43bef769b09ce724e74e619d5beff22d
Rickson-Rocha <ricksonroccha21@gmail.com>
2023-04-26 20:57:38 -0300

    implementando  o método is_empty

---
+++ pilha/no.py
@@ -0,0 +1,7 @@
+from typing import Any
+
+
+class No:
+    def __init__(self,valor:Any) -> None:
+        self.__valor =  valor
+        self.__prox = None
---
+++ pilha/pilha.py
@@ -0,0 +1,27 @@
+from no import No
+
+
+class Pilha:
+    def __init__(self) -> None:
+        self.topo = None
+        self.tamanho = 0
+    
+    def is_empty(self):
+        if self.tamanho == 0 :
+            return True
+        return False
+
+    def push(self):
+        pass
+    
+    def pop(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def list_items(self):
+        pass
+
+    def get_size(self):
+        pass
